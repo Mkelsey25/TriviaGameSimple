dc74cd092666a8957939cafbfc02a8827c9f7e58
Mkelsey <mkh13b@my.fsu.edu>
2018-06-16 15:43:08 -0400

    It works and has a start page

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,5 +1,31 @@
+$(document).ready(function() {
+$(".container").on("click", ".startGame", function() {
+    //Dynamically create questions in the html
+    $(".col-md-8").append("<h4>Which of these beers is considered a dark beer?</h4>");
+    $(".col-md-8").append("<form>");
+    $("form").attr("id", "form");
+    $("#form").append(('<input type="radio" class="C" name="darkBeer" value="C" />' + "Stout" + "<br>"));
+    $("#form").append(('<input type="radio" class="W" name="darkBeer" value="W" />' + "Wheat Ale" + "<br>"));
+    $("#form").append(('<input type="radio" class="W" name="darkBeer" value="W" />' + "American Pale Ale" + "<br>"));
+    $("#form").append("<h4>When did humans begin brewing beer?</h4>");
+    $("#form").append(('<input type="radio" class="W" name="year" value="W" />' + "6000 years ago" + "<br>"));
+    $("#form").append(('<input type="radio" class="C" name="year" value="C" />' + "3900 years ago" + "<br>"));
+    $("#form").append(('<input type="radio" class="W" name="year" value="W" />' + "1580 years ago" + "<br>"));
+    $("#form").append("<h4>Which of these is a brewery located in Atlanta?</h4>");
+    $("#form").append(('<input type="radio" class="W" name="local" value="W" />' + "Brekenridge" + "<br>"));
+    $("#form").append(('<input type="radio" class="W" name="local" value="W" />' + "Bell's" + "<br>"));
+    $("#form").append(('<input type="radio" class="C" name="local" value="C" />' + "Second Self" + "<br>"));
+    $("#form").append("<h4>Which of these beers is considered a sour?</h4>");
+    $("#form").append(('<input type="radio" class="W" name="sour" value="W" />' + "Porter" + "<br>"));
+    $("#form").append(('<input type="radio" class="W" name="sour" value="W" />' + "Pilsner" + "<br>"));
+    $("#form").append(('<input type="radio" class="C" name="sour" value="C" />' + "Gose"));
+    $(".col-md-8").append("<button>Submit</button>");
+    $("button").attr("id", "submit");
 
-var seconds = 40;
+    run();
+});
+
+var seconds = 15;
 var intervalId;
 var wrongAnswers = 0;
 var correctAnswers = 0;
@@ -72,7 +98,8 @@ clearInterval(intervalId);
                 $(".container").html("<style> * { text-align: center;} </style>" + "<br> <h1>Beer Snob Trivia</h1> <br> <br> <br> <h2>Correct Answers:" + correctAnswers + "</h2>" + "<h2> Wrong Answers:" + wrongAnswers + "</h2>" + "<h2> Unanswered:" + unanswered + "</h2> <br> <br> <br> <br> <br> <br> <br> <br> <br> <br> <br> <br>");
 
             });
-            run();
+        });
+
 
                  
            
